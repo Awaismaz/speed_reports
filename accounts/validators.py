b78d1e67326db872f3924bb09025f80a2bb41727
Awaismaz <awais_mazahir@hotmail.com>
2024-04-28 22:14:42 +0500

    Corrected validator

--- accounts/validators.py
+++ accounts/validators.py
@@ -2,13 +2,19 @@ from django.contrib.auth.password_validation import UserAttributeSimilarityValid
 from django.core.exceptions import ValidationError
 
 class CustomAttributeSimilarityValidator(UserAttributeSimilarityValidator):
-    def get_help_text(self):
-        return 'Avoid using your name or other personal information as your password.'
-    
     def validate(self, password, user=None):
-        super().validate(password, user)
-        if self.get_help_text() in self.error_list:
-            raise ValidationError(
-                'Avoid using your name or other personal information as your password.',
-                code='password_too_similar',
-            )
+        try:
+            super().validate(password, user)
+        except ValidationError as e:
+            # Check if the default error message is in the exception
+            if 'The password is too similar to the' in str(e):
+                raise ValidationError(
+                    'Avoid using your name or other personal information as your password.',
+                    code='password_too_similar',
+                )
+            else:
+                # If the error is not about attribute similarity, re-raise the original error
+                raise e
+
+    def get_help_text(self):
+        return 'Your password should not be too similar to your other personal information.'
